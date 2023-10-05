b1c7a1af82c4d294b0894f03282d3377d588f227
Zed Kaido <ekai@tuta.io>
2023-10-05 11:41:35 +0100

    update `snippets.lua` (`,` interfered with in-line search)

--- lua/zk/snippets.lua
+++ lua/zk/snippets.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("n", ",l", ":normal! aλ<CR>", { remap = true })
-vim.keymap.set("n", ",shr", ":-1read ~/.snippets/shrug<CR>", { remap = true })
-vim.keymap.set("n", ",html", ":-1read ~/.snippets/sk-html.html<CR>3jwf>a", { remap = true })
+vim.keymap.set("n", "<leader>il", ":normal! aλ<CR>", { remap = true })
+vim.keymap.set("n", "<leader>ishr", ":-1read ~/.snippets/shrug<CR>", { remap = true })
+vim.keymap.set("n", "<leader>ihtml", ":-1read ~/.snippets/sk-html.html<CR>3jwf>a", { remap = true })
