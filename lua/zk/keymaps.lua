a649387d6835ebd019475cb9c9bef7253cc706af
Zed Kaido <ekai@tuta.io>
2023-11-17 08:18:09 +0000

    add `<leader>nr` keymap that deletes surrounding bracket links

--- lua/zk/keymaps.lua
+++ lua/zk/keymaps.lua
@@ -68,5 +68,6 @@ vim.keymap.set("n", "<leader>cge", ":ChatGPTEditWithInstructions<CR>", { desc = 
 vim.keymap.set("n", "<leader>cgr", ":ChatGPTRun ", { desc = "[C]hat[G]PT [R]un" })
 vim.keymap.set("v", "<leader>cgr", ":ChatGPTRun ", { desc = "[C]hat[G]PT [R]un" })
 
-vim.keymap.set("i", "<C-l>", "<Esc>ysiW]wysiW]eeea<Space>", { desc = "Surround word with [[x]]", remap = true })
-vim.keymap.set("n", "<leader>nl", "ysiW]wysiW]w", { desc = "Surround word with [[x]]", remap = true })
+vim.keymap.set("i", "<C-l>", "<Esc>ysiW]wysiW]eeea<Space>", { desc = "`x` -> [[x]]", remap = true })
+vim.keymap.set("n", "<leader>nl", "ysiW]wysiW]w", { desc = "`x` -> [[x]]", remap = true })
+vim.keymap.set("n", "<leader>nr", "ds]ds]", { desc = "[[x]] -> `x`", remap = true })
