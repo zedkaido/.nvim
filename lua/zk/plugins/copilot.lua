c686093374f0c74ba5f954a532713ca273499d51
Zed Kaido <z@zedkaido.com>
2023-12-24 22:04:17 +0000

    update `copilot` keymap

--- lua/zk/plugins/copilot.lua
+++ lua/zk/plugins/copilot.lua
@@ -24,7 +24,7 @@ return {
 				auto_trigger = false,
 				debounce = 75,
 				keymap = {
-					accept = "<C-y>",
+					accept = "<M-y>",
 					accept_word = false,
 					accept_line = false,
 					next = "<C-j>",
