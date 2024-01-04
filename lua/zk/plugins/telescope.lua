628abd78542663374b51c28eb7af1ca26b7867c9
Zed Kaido <z@zedkaido.com>
2024-01-04 21:34:29 +0000

    quality of life word search

--- lua/zk/plugins/telescope.lua
+++ lua/zk/plugins/telescope.lua
@@ -14,9 +14,9 @@ return {
 			end,
 		},
 	},
-	config = function ()
+	config = function()
 		local telescope = require("telescope")
-		local telescope_builtin = require("telescope.builtin")
+		local builtin = require("telescope.builtin")
 		local ts_themes = require("telescope.themes")
 
 		-- Enable telescope fzf native, if installed
@@ -26,36 +26,48 @@ return {
 		function ResumeTelescope()
 			if telescope_last == 0 then
 				telescope_last = 1
-				telescope_builtin.live_grep()
+				builtin.live_grep()
 			else
-				telescope_builtin.resume()
+				builtin.resume()
 			end
 		end
 
 		vim.keymap.set("n", "<leader>l", ResumeTelescope)
-		vim.keymap.set("n", "<leader>?", telescope_builtin.oldfiles, { desc = "[?] Find recently opened files" })
-		vim.keymap.set("n", "<leader><space>", telescope_builtin.buffers, { desc = "[ ] Find existing buffers" })
+		vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
+		vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 		vim.keymap.set("n", "<leader>/", function()
 			-- You can pass additional configuration to telescope to change theme, layout, etc.
 			require("telescope.builtin").current_buffer_fuzzy_find(ts_themes.get_dropdown {
 				previewer = false,
 			})
-			end, { desc = "[/] Fuzzily search in current buffer" })
-		vim.keymap.set("n", "<leader>s/", function()
-			telescope_builtin.grep_string({ search = vim.fn.input("Grep > ") });
-			end, { desc = "[/] Grep project for a string" })
+		end, { desc = "[/] Fuzzily search in current buffer" })
 
-		vim.keymap.set("n", "<leader>gf", telescope_builtin.git_files, { desc = "Search [G]it [F]iles" })
-		vim.keymap.set("n", "<leader>sf", telescope_builtin.find_files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>sh", telescope_builtin.help_tags, { desc = "[S]earch [H]elp" })
-		vim.keymap.set("n", "<leader>sw", telescope_builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sg", telescope_builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>sd", telescope_builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+		vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Search [G]it [F]iles" })
+		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+
+		vim.keymap.set("n", "<leader>sw", function()
+				local word = vim.fn.expand("<cword>")
+				builtin.grep_string({ search = word })
+			end,
+			{ desc = "[S]earch current [W]ord" }
+		)
+		vim.keymap.set("n", "<leader>sW", function()
+				local word = vim.fn.expand("<cWORD>")
+				builtin.grep_string({ search = word })
+			end,
+			{ desc = "[S]earch whole current [W]ord" }
+		)
+		vim.keymap.set("n", "<leader>s/", function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") });
+		end, { desc = "[/] Grep project for a string" })
 
 		telescope.setup({
-			defaults = {
+			defaults = require('telescope.themes').get_ivy {
 				file_ignore_patterns = { "node_modules", "static" },
-			}
+			},
 		})
 	end
 }
